e9ef6a43efeff939e672be8a5cfcda09af43161d
umibozu <umibozu@ff506a72-e447-0410-a6ab-aa1658c7dca5>
2010-04-20 09:21:04 +0000

    Italian messages

--- trunk/src/com/fbergeron/organigram/view/OrganigramEventManager.java
+++ trunk/src/com/fbergeron/organigram/view/OrganigramEventManager.java
@@ -1,17 +1,18 @@
-/** LGPL > 3.0
- * Copyright (C) 2005 Frédéric Bergeron (fbergeron@users.sourceforge.net)
- * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
- *
+/**
+ * LGPL > 3.0 Copyright (C) 2005 Frédéric Bergeron
+ * (fbergeron@users.sourceforge.net) Copyright (C) 2006-2010 eIrOcA (eNrIcO
+ * Croce & sImOnA Burzio)
+ * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/
  */
@@ -132,7 +133,10 @@ public class OrganigramEventManager extends MouseAdapter implements MouseMotionL
     currentLink = null;
     if (unitView != null) {
       if (unitView.canExpand()) {
-        tipText = Messages.getString("RightClick"); //$NON-NLS-1$
+        tipText = Messages.getString("RightClick1"); //$NON-NLS-1$
+      }
+      else if (unitView.canCollapse()) {
+        tipText = Messages.getString("RightClick2"); //$NON-NLS-1$
       }
       final Unit unit = unitView.getUnit();
       final String link = unit.getMeta("link"); //$NON-NLS-1$
